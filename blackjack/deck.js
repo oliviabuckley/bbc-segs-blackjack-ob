d23b7a6842726be4d5362d6b5b55b4ea185441d0
Olivia Buckley <olivia.buckley7898@gmail.com>
2024-12-24 16:25:39 +0000

    add shuffle function for the deck and tests

--- blackjack/deck.js
+++ blackjack/deck.js
@@ -29,6 +29,12 @@ class Deck {
     }
     return deck;
   }
+  shuffleDeck() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
 }
 
 module.exports = Deck;
