f7948b4ba6f67f1eb04d362b1c0732b4d1790802
Olivia Buckley <olivia.buckley7898@gmail.com>
2024-12-27 11:54:10 +0000

    add remaining hand methods and implement tests

--- __tests__/hand.test.js
+++ __tests__/hand.test.js
@@ -15,4 +15,90 @@ describe("Hand", () => {
       expect(hand.cards[0]).toBe(card);
     });
   });
+  describe("getHandValue", () => {
+    test("return 0 for an empty hand", () => {
+      const hand = new Hand();
+      expect(hand.getHandValue()).toBe(0);
+    });
+    test("return the correct value of cards in the hand", () => {
+      const hand = new Hand();
+      const card1 = new Card("3", "Clubs");
+      const card2 = new Card("8", "Diamonds");
+      hand.addCard(card1);
+      hand.addCard(card2);
+      expect(hand.getHandValue()).toBe(11);
+    });
+    test("count Ace as 11 if total value is under 21", () => {
+      const hand = new Hand();
+      hand.addCard(new Card("Ace", "Spades"));
+      hand.addCard(new Card("5", "Diamonds"));
+      expect(hand.getHandValue()).toBe(16);
+    });
+    test("count Ace as 1 if total value is over 21", () => {
+      const hand = new Hand();
+      hand.addCard(new Card("Ace", "Spades"));
+      hand.addCard(new Card("5", "Diamonds"));
+      hand.addCard(new Card("10", "Clubs"));
+      expect(hand.getHandValue()).toBe(16);
+    });
+    test("handle multiple Aces correctly", () => {
+      const hand = new Hand();
+      const card1 = new Card("Ace", "Spades");
+      const card2 = new Card("Ace", "Diamonds");
+      hand.addCard(card1);
+      hand.addCard(card2);
+      expect(hand.getHandValue()).toBe(12);
+    });
+  });
+  describe("isBust", () => {
+    test("return true if value of hand exceeds 21", () => {
+      const hand = new Hand();
+      const card1 = new Card("8", "Clubs");
+      const card2 = new Card("10", "Diamonds");
+      const card3 = new Card("9", "Spades");
+      hand.addCard(card1);
+      hand.addCard(card2);
+      hand.addCard(card3);
+      expect(hand.isBust()).toBe(true);
+    });
+    test("return false if value of hand does not exceed 21", () => {
+      const hand = new Hand();
+      const card1 = new Card("8", "Clubs");
+      const card2 = new Card("10", "Diamonds");
+      hand.addCard(card1);
+      hand.addCard(card2);
+      expect(hand.isBust()).toBe(false);
+    });
+  });
+  describe("hasBlackjack", () => {
+    test("return true for a blackjack hand)", () => {
+      const hand = new Hand();
+      const card1 = new Card("Ace", "Clubs");
+      const card2 = new Card("King", "Diamonds");
+      hand.addCard(card1);
+      hand.addCard(card2);
+      expect(hand.hasBlackjack()).toBe(true);
+    });
+    test("return false for a score of 21 from more than 2 cards", () => {
+      const hand = new Hand();
+      const card1 = new Card("10", "Clubs");
+      const card2 = new Card("10", "Diamonds");
+      const card3 = new Card("1", "Diamonds");
+      hand.addCard(card1);
+      hand.addCard(card2);
+      hand.addCard(card3);
+      expect(hand.hasBlackjack()).toBe(false);
+    });
+  });
+  describe("resetHand", () => {
+    test("remove all cards from hand", () => {
+      const hand = new Hand();
+      const card1 = new Card("3", "Clubs");
+      const card2 = new Card("8", "Diamonds");
+      hand.addCard(card1);
+      hand.addCard(card2);
+      hand.resetHand();
+      expect(hand.cards.length).toBe(0);
+    });
+  });
 });
--- blackjack/hand.js
+++ blackjack/hand.js
@@ -1,12 +1,35 @@
 const Card = require("./card");
-const Deck = require("./deck");
 
 class Hand {
   constructor() {
     this.cards = [];
   }
   addCard(card) {
-    this.cards.push(card);
+    if (card instanceof Card) {
+      this.cards.push(card);
+    }
+  }
+  getHandValue() {
+    let totalValue = 0;
+    let aceCount = 0;
+    this.cards.forEach((card) => {
+      totalValue += card.getValue();
+      if (card.rank === "Ace") aceCount++;
+    });
+    while (totalValue > 21 && aceCount > 0) {
+      totalValue -= 10;
+      aceCount--;
+    }
+    return totalValue;
+  }
+  isBust() {
+    return this.getHandValue() > 21;
+  }
+  hasBlackjack() {
+    return this.cards.length === 2 && this.getHandValue() === 21;
+  }
+  resetHand() {
+    this.cards = [];
   }
 }
 
