edc2d7d9a9da5567857b992fe3ced480965760dc
Olivia Buckley <olivia.buckley7898@gmail.com>
2024-12-30 10:27:29 +0000

    add determineWinner method to Game class with tests

--- blackjack/game.js
+++ blackjack/game.js
@@ -32,6 +32,7 @@ class Game {
     if (this.gameOver) return;
     if (action === "hit") {
       this.player.addCard(this.deck.drawCard());
+      console.log("Player's Hand: " + this.player.showHand());
       if (this.player.isBusted()) {
         this.gameOver = true;
         return "Player Busts";
@@ -39,18 +40,53 @@ class Game {
       return "Player Hits";
     }
     if (action === "stand") {
+      console.log("Player's Hand: " + this.player.showHand());
       return "Player Stands";
     }
   }
   dealerTurn() {
     if (this.gameOver) return;
     this.dealer.playTurn(this.deck);
+    console.log("Dealer's Hand: " + this.dealer.showHand());
     if (this.dealer.isBusted()) {
       this.gameOver = true;
       return "Dealer Busts";
     }
     return "Dealer Finishes Turn";
   }
+  determineWinner() {
+    const playerHandValue = this.player.hand.getHandValue();
+    const dealerHandValue = this.dealer.hand.getHandValue();
+
+    console.log("Player's Hand Value: " + playerHandValue);
+    console.log("Dealer's Hand Value: " + dealerHandValue);
+
+    if (playerHandValue > 21) {
+      return "Dealer Wins (Player Busts)";
+    } else if (dealerHandValue > 21) {
+      return "Player Wins (Dealer Busts)";
+    } else if (playerHandValue > dealerHandValue) {
+      return "Player Wins";
+    } else if (dealerHandValue > playerHandValue) {
+      return "Dealer Wins";
+    } else {
+      return "It's a Tie";
+    }
+  }
+}
+
+const game = new Game();
+game.startGame();
+
+// Player turn
+console.log(game.playerTurn("hit")); // or "stand"
+
+// Dealer turn
+console.log(game.dealerTurn());
+
+// Determine winner
+if (game.gameOver) {
+  console.log(game.determineWinner());
 }
 
 module.exports = Game;
